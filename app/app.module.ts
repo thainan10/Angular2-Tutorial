8356cda17f71a585ff58948a930a131fc2e87508
Thainan Matias <thainan10@gmail.com>
2016-09-22 17:08:51 -0300

    Added the mock API and access of it.
    It was added the mock api to get the heroes data and the hero service is getting the promise data from that.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -15,6 +19,8 @@ import { routing } from './app.routing';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     routing
   ],
   declarations: [
